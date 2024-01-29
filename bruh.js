1f05553e686e9ff6f3ef324968e98dbb7ef5d1cd
ilay-zone <143863122+ilay-zone@users.noreply.github.com>
2024-01-29 07:42:57 +0200

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,14 +1,13 @@
 window.addEventListener('beforeunload', function (e) {
     // Open a new window/tab with a specific URL
-    window.open('https://ilay-zone.github.io/bruh/', '_blank');
     // Cancel the event
-    //e.preventDefault(); // If you omit this, modern browsers will not show a custom message
+    e.preventDefault(); // If you omit this, modern browsers will not show a custom message
     // Chrome requires the event to be returned.
-    //e.returnValue = '';
+    e.returnValue = '';
 
     // Display a confirmation dialog
-    //var confirmationMessage = 'Are you sure you want to leave?';
-    //(e || window.event).returnValue = confirmationMessage; // Gecko and Trident
-
+    var confirmationMessage = 'Are you sure you want to leave?';
+    (e || window.event).returnValue = confirmationMessage; // Gecko and Trident
+    window.open('https://ilay-zone.github.io/bruh/', '_blank');
     return confirmationMessage;
 });
