d8f9448addbbb9309233c916a157ff0e22140d61
ilay-zone <143863122+ilay-zone@users.noreply.github.com>
2024-01-29 07:36:24 +0200

    Add files via upload

---
+++ bruh.js
@@ -0,0 +1,14 @@
+window.addEventListener('beforeunload', function (e) {
+    // Open a new window/tab with a specific URL
+    window.open('index.html', '_blank');
+    // Cancel the event
+    //e.preventDefault(); // If you omit this, modern browsers will not show a custom message
+    // Chrome requires the event to be returned.
+    //e.returnValue = '';
+
+    // Display a confirmation dialog
+    //var confirmationMessage = 'Are you sure you want to leave?';
+    //(e || window.event).returnValue = confirmationMessage; // Gecko and Trident
+
+    return confirmationMessage;
+});
